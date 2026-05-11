5dab7322cd158de0438b2cc1fe4bbafb09b3c891
[Vaishnavi-Powar-01] <[vaishnavipowar11@gmail.com]>
2026-05-11 10:39:41 +0530

    fix db connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,19 @@
 
 const mysql = require("mysql2");
 
-const connection = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.log("Database Connection Failed", err);
   } else {
-    console.log("MySQL Connected");
+    console.log("MySQL Connected (Pool)");
   }
 });
 
