5ff601187311b8cce1406dd0971d800531ebd806
[Vaishnavi-Powar-01] <[vaishnavipowar11@gmail.com]>
2026-05-12 14:02:26 +0530

    fixed

--- src/app.js
+++ src/app.js
@@ -23,7 +23,11 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/uploads", express.static("uploads"));
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "uploads"))
+);
+ 
 
 /* ===================== IMPORT ROUTES ===================== */
 
